4480a10e6801cc1827488a78102850776b61214d
ahmed zidan <zidanzezo17@gmail.com>
2024-05-15 15:07:59 +0300

    first

--- WebApi/WeatherForecast.cs
+++ WebApi/WeatherForecast.cs
@@ -10,6 +10,6 @@ namespace WebApi
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
-        public string Summary1 { get; set; }
+        public string Summary { get; set; }
     }
 }
